7b3215e725afb038d51f7d4594aedf57cba3fedf
Shlok Vaidya <152369382+ShlokVaidya@users.noreply.github.com>
2025-03-15 23:36:20 +0000

    hero section created

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider>
-          {children}
           <Navbar />
+          {children}
         </ThemeProvider>
         <Analytics />
       </body>
